c0bb11186255b66fe0cbbdeb248dfe767479929e
tinny <info@tinnyhq.com>
2013-05-01 10:15:16 +1000

    ems: changed modified by name

--- mods/ems/ems.lua
+++ mods/ems/ems.lua
@@ -2,7 +2,7 @@ LICENSE = [[
 mod_server.lua
 
 Copyright (c) 2013 Phil Hassey
-Modifed by: YOUR_NAME_HERE
+Modifed by: the batman
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
